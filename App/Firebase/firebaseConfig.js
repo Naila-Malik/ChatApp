bbe0f99d79fbd7f4483cf1c6b6ee358e067563d9
Naila Malik <Naila.Malik@sagacious.local>
2022-08-02 05:35:54 -0700

    first commit

---
+++ App/Firebase/firebaseConfig.js
@@ -0,0 +1,10 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyB2GuweGDpE4qQP93q1zvjmXxrBvUFxlKg',
+  databaseURL: '',
+  projectId: '',
+  appId: '',
+};
+
+export default firebase.initializeApp(firebaseConfig);
